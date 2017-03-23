915a111dd788b59c358a0c446481405eb5028df3
Jordan Mavrogeorge <jordomav@gmail.com>
2017-03-22 18:57:24 -0700

    scrape html from Wikipedia

--- scrape/views.py
+++ scrape/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
+import requests
 
 # client = MongoClient('mongodb://localhost::27017/')
 # db = client.scraper
 # collection = db.posts
+
+
+def find(val):
+    search = requests.get("https://en.wikipedia.org/wiki/" + val)
+    c = search.content
+    soup = BeautifulSoup(c, 'html.parser')
+    return soup.prettify()
 
 
 def index(request):
@@ -13,7 +22,7 @@ def index(request):
 
 
 def scraped(request, post_body):
-    context = {'body': post_body}
+    context = {'body': post_body, 'res': find(post_body)}
     return render(request, 'scrape/results.html', context)
 
 
